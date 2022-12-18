c5b72acde1fae040df5decb80e2dfc75e3e7f657
Ivan Tkachuk <ivtkac@gmail.com>
2022-12-18 22:32:34 +0200

    add more aliases for fish

--- config/fish/conf.d/alias.fish
+++ config/fish/conf.d/alias.fish
@@ -1,11 +1,13 @@
-# Git
+# Git aliases
 alias gcb='git checkout -b'
 alias gco='git checkout'
+alias gcm='git checkout main'
 alias gcd='git checkout dev'
 alias gst='git status'
 alias gac='git add -A && git commit -v'
 alias guc='git add -u && git commit -v'
 
+alias gb='git branch'
 alias gp='git push'
 alias gpl='git pull'
 alias gf='git fetch'
@@ -13,9 +15,23 @@ alias gf='git fetch'
 alias gcp='git cherry-pick'
 alias gcpno='git cherry-pick --no-commit'
 
-# Zellij
+# Zellij aliases
 alias zr='zellij run --'
 alias ze='zellij edit'
 
+# Command aliases
+
 alias vim='hx'
 alias tmux='zellij'
+
+# Skim aliases
+
+alias sgb='git branch --sort=-committerdate | sk --header "Checkout Recent Branch" --preview "git diff --color=always {1}" | xargs git checkout'
+
+# Pacman aliases
+
+alias upg='ame'
+alias upgrepo='ame upg --repo'
+alias upgaur='ame upg --aur'
+alias unlock='sudo rm /var/lib/pacman/db.lck'
+alias cleanup='pacman -Qtdq | sudo pacman -Rns -'
