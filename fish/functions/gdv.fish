f20994709113b69da0e387bddabde1298db20081
Ivan Tkachuk <ivtkac@gmail.com>
2023-03-13 02:08:36 +0200

    remove fish config

--- fish/functions/gdv.fish
+++
@@ -1,3 +0,0 @@
-function gdv -w "git diff -w" -d "Pipe `git diff` to `hx` editor"
-  git diff -w $argv | hx -
-end
--- fish/functions/snap.fish
+++
@@ -1,3 +0,0 @@
-function snap --wraps='ame ins base' --wraps='ame ins vim --no-confirm' --wraps='ame ins vim --noconfirm' --description 'alias snap=ame ins vim --noconfirm'
-  ame ins vim --noconfirm $argv; 
-end
