c86f7daad099b45a71a4970f8def20dd1730a95e
Ivan Tkachuk <ivtkac@gmail.com>
2023-05-06 08:21:25 +0300

    add installing gstreamer plugins for arch script

--- scripts/arch/install.sh
+++ scripts/arch/install.sh
@@ -25,6 +25,10 @@ flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flat
 
 sudo pacman -S --noconfirm --needed wine gnome-tweaks zoxide bat exa helix starship
 
+sudo pacman -S --noconfirm --needed gst-libav gst-plugins-{bad,base,good,ugly} gstreamer-vaapi gst-plugin-pipewire
+
+yay -S --noconfirm --needed gst-plugin-libde265 gst-plugins-openh264
+
 yay -S --noconfirm --needed adw-gtk3 visual-studio-code-bin
 
 flatpak install flathub -y --noninteractive < flatpak.txt
