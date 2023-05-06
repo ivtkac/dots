f85fcd241cf99ce2701f05fc7277497f6cadbe00
Ivan Tkachuk <ivtkac@gmail.com>
2023-05-06 04:35:51 +0000

    Update dotfiles

--- install.sh
+++ install.sh
@@ -1,24 +1,17 @@
 #!/usr/bin/env bash
 
-# Sure that folder is exists
+# Install from home to ~/
+
+for file in home/*; do
+    ln -s $PWD/$file ~/.$(basename $file)
+done
+
+
+# Install from config to ~/.config
+
 mkdir -p ~/.config
 
-# Unlink all existing files
-links=$(find ~/.config -type l)
-
-for linkfile in "$links"; do
-  unlink "$linkfile"
+for file in config/*; do
+    ln -s $PWD/$file ~/.config/$(basename $file)
 done
 
-# Link all dotfiles to those in this repo.
-for rcfile in "$PWD/config"; do
-  ln -fsnv "$rcfile" "~/.config/${rcfile:t}"
-done
-
-# Install bash scripts
-for bashfile in "$PWD/.bash*"; do
-  ln -fsnv "$PWD/$bashfile" "~/${bashfile:t}"
-done
-
-# Install arch packages
-pacman -S --needed $installable_packages
