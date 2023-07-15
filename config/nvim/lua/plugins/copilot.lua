987a32668287f65a6970e1d26c4f6cd5a6700a86
Ivan Tkachuk <ivtkac@gmail.com>
2023-07-16 01:11:36 +0300

    Update dotfiles

--- config/nvim/lua/plugins/copilot.lua
+++
@@ -1,8 +0,0 @@
-return {
-  {
-    "github/copilot.vim",
-    config = function()
-      vim.g.copilot_assume_mapped = true
-    end,
-  },
-}
--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 # Install from home to ~/
-
 for file in home/*; do
     ln -s $PWD/$file ~/.$(basename $file)
     echo "Successfully installed ~/.$(basename $file)"
@@ -16,19 +15,3 @@ for file in config/*; do
     ln -s $PWD/$file ~/.config/$(basename $file)
     echo "Successfully installed ~/.config/$(basename $file)"
 done
-
-# Prompt for OS-specific scripts if press yes then execute
-
-read -p "Do you want to install OS-specific scripts? [y/N] " -n 1 -r
-echo ""
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-    if [[ $(hostname) == "arch" ]]; then
-        for file in scripts/arch/*; do
-            bash $PWD/$file
-        done
-    elif [[ $(hostname) == "fedora" ]]; then
-        for file in scripts/fedora/*; do
-            bash $PWD/$file
-        done
-    fi
-fi
