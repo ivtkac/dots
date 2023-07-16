a3415fbdd1f7fa7f5c892d38bf71919f02ba16a4
Ivan Tkachuk <ivtkac@gmail.com>
2023-07-16 08:32:30 +0300

    Add flake.nix for dev environment

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Development environment for my dotfiles";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [ 
+            shellcheck
+            nodePackages.bash-language-server
+         ];
+        };
+      });
+    };
+}
