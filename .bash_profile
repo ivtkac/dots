c62214ddd07b60f0806eb8ae3f2ad9c8368c5510
Ivan Tkachuk <ivtkac@gmail.com>
2023-12-30 08:25:33 +0200

    Update dots

--- .bash_profile
+++ .bash_profile
@@ -8,4 +8,5 @@ if [[ -n "$BASH" ]]; then
   case $- in
     *i*) source "$HOME/.bashrc" ;;
       *) return ;;
+  esac
 fi
--- config/fish/functions/gl.fish
+++
@@ -1,5 +0,0 @@
-function gl -d "git log at requested pretty level" -a format
-  set -q format[1]; and git log --pretty=$format
-end
-
-complete -c gl -x -a "(complete -C 'git log --pretty=' | sed 's/^--pretty=//')"
