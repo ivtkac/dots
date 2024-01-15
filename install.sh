3547d2112f6bb721537c3efac500032fa74a54e4
Ivan Tkachuk <ivtkac@gmail.com>
2024-01-15 18:29:52 +0200

    Add nvim config

--- install.sh
+++ install.sh
@@ -3,25 +3,25 @@
 CONFIG_DIR=${XDG_CONFIG_DIR:-$HOME/.config}
 
 install_files() {
-  source=$1
-  destination=$2
+	source=$1
+	destination=$2
 
-  mkdir -p "$destination"
+	mkdir -p "$destination"
 
-  shopt -s dotglob
+	shopt -s dotglob
 
-  for file in "$source"/*; do
-    filename=$(basename "$file")
-    
-    echo "$file -> $destination/$filename"
-    ln -sf "$file" "$destination/$filename"
-  done
+	for file in "$source"/*; do
+		filename=$(basename "$file")
+
+		echo "$file -> $destination/$filename"
+		ln -sf "$file" "$destination/$filename"
+	done
 }
 
 install_dots() {
-  install_files "$PWD/home" "$HOME"
-  install_files "$PWD/config" "$CONFIG_DIR"
-  install_files "$PWD/local" "$HOME/.local"
+	install_files "$PWD/home" "$HOME"
+	install_files "$PWD/config" "$CONFIG_DIR"
+	install_files "$PWD/local" "$HOME/.local"
 }
 
 install_dots
