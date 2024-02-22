9d4ef35efeed6bac2d84f720ec60fbfea8b1c952
Ivan Tkachuk <ivtkac@gmail.com>
2024-02-22 13:24:19 +0200

    Update dots

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -1,8 +1,16 @@
-local keymap = vim.keymap
+vim.keymap.set("n", "<C-k>", ":wincmd k<CR>")
+vim.keymap.set("n", "<C-j>", ":wincmd j<CR>")
+vim.keymap.set("n", "<C-h>", ":wincmd h<CR>")
+vim.keymap.set("n", "<C-l>", ":wincmd l<CR>")
 
-keymap.set("n", "<C-k>", ":wincmd k<CR>")
-keymap.set("n", "<C-j>", ":wincmd j<CR>")
-keymap.set("n", "<C-h>", ":wincmd h<CR>")
-keymap.set("n", "<C-l>", ":wincmd l<CR>")
+vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>")
 
-keymap.set("n", "<leader>h", ":nohlsearch<CR>")
+-- Remap for dealing with word warp
+vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
+-- Diagnostic keymaps
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
