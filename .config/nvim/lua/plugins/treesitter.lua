b81d309df9ac8a4851fbe52dedd06d520be7b34e
Ivan Tkachuk <ivtkac@gmail.com>
2024-02-22 06:04:07 +0200

    Add formatting and diagnostics to nvim

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
   config = function()
     local config = require("nvim-treesitter.configs")
     config.setup({
-      ensure_instaleld = { "lua", "javascript", "python", "toml", "bash", "html", "vim" },
+      auto_install = true,
       highlight = { enable = true },
       indent = { enable = true },
     })
