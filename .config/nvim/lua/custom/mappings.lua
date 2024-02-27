3efcd2c28b2217d58ba0a3a6d399ca1d18a5d6c9
Ivan Tkachuk <ivtkac@gmail.com>
2024-02-27 17:57:36 +0200

    Format neovim config

--- .config/nvim/lua/custom/mappings.lua
+++ .config/nvim/lua/custom/mappings.lua
@@ -2,43 +2,43 @@
 local M = {}
 
 M.general = {
-	n = {
-		[";"] = { ":", "enter command mode", opts = { nowait = true } },
+  n = {
+    [";"] = { ":", "enter command mode", opts = { nowait = true } },
 
-		--  format with conform
-		["<leader>fm"] = {
-			function()
-				require("conform").format()
-			end,
-			"formatting",
-		},
+    --  format with conform
+    ["<leader>fm"] = {
+      function()
+        require("conform").format()
+      end,
+      "formatting",
+    },
 
-		["<leader>co"] = {
-			function()
-				vim.lsp.buf.code_action({
-					apply = true,
-					context = {
-						only = { "source.organizeImports" },
-						diagnostics = {},
-					},
-				})
-			end,
-			"Organize imports",
-		},
-	},
+    ["<leader>co"] = {
+      function()
+        vim.lsp.buf.code_action {
+          apply = true,
+          context = {
+            only = { "source.organizeImports" },
+            diagnostics = {},
+          },
+        }
+      end,
+      "Organize imports",
+    },
+  },
 
-	v = {
-		[">"] = { ">gv", "indent" },
-	},
+  v = {
+    [">"] = { ">gv", "indent" },
+  },
 }
 
 -- more keybinds!
 M.telescope = {
-	plugin = true,
-	n = {
-		["gs"] = { "<cmd> Telescope lsp_document_symbols <CR>", "Document LSP Symbols" },
-		["gS"] = { "<cmd> Telescope lsp_workspace_symbols <CR>", "Workspace LSP Symbols" },
-	},
+  plugin = true,
+  n = {
+    ["gs"] = { "<cmd> Telescope lsp_document_symbols <CR>", "Document LSP Symbols" },
+    ["gS"] = { "<cmd> Telescope lsp_workspace_symbols <CR>", "Workspace LSP Symbols" },
+  },
 }
 
 return M
