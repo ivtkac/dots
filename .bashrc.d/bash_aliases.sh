1163a6e23f4282ee535badbeac94f6ad3aa4705e
Ivan Tkachuk <ivtkac@gmail.com>
2024-03-05 19:54:36 +0200

    Add some bash aliases

--- .bashrc.d/bash_aliases.sh
+++ .bashrc.d/bash_aliases.sh
@@ -1,26 +1,29 @@
 #!/usr/bin/env bash
 
-alias ls='eza --icons --color-scale all'
-alias ll='ls -lah'
-alias l.='ls -d .*'
+alias ls='eza --color-scale all'
+alias ll='eza --icons -lah'
+alias l.='eza -d .*'
+
+alias cat='bat -pp'
+
+alias vi='nvim'
+alias vim='nvim'
 
 alias less="less -R"
 
 alias ..='cd ..'
 alias ...="cd ../.."
 
-alias proj='cd ~/Projects'
-alias dots='cd ~/.dotfiles'
-
-alias bat='bat --theme "Catppuccin"'
-
-alias ports='netstat -tulanp'
-
 alias cpuinfo='lscpu'
 alias meminfo="free -h"
 
 alias sys="systemctl"
 alias src="source $HOME/.bashrc"
+
+
+alias upd='paru -Syu'
+alias fcd='cd $(fd -t d | fzf)'
+alias cpwd='pwd | tr -d "\n" | c'
 
 alias c="wl-copy"
 alias v="nvim"
@@ -29,4 +32,3 @@ alias t="type"
 alias b="bat -p"
 alias l="ls -l"
 
-alias cpwd='pwd | tr -d "\n" | c'
