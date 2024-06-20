70c12fe3339ed675f4acecc628e940f759355bf9
Ivan Tkachuk <ivtkac@gmail.com>
2024-06-20 05:03:26 +0300

    remove alias fd ans grep, and add functions for kill proc and edit file

--- .config/bash/bashrc.d/bash_aliases.sh
+++ .config/bash/bashrc.d/bash_aliases.sh
@@ -8,19 +8,8 @@ alias l.='eza -d .*' 2>/dev/null
 alias ll='eza -l --icons=auto --group-directories-first' 2>/dev/null
 alias l1='eza -1'
 
-# Ripgrep for grep
-alias grep='rg' 2>/dev/null
-alias egrep='rg' 2>/dev/null
-alias fgrep='rg -F' 2>/dev/null
-alias xzgrep='rg -z' 2>/dev/null
-alias xzegrep='rg -z' 2>/dev/null
-alias xzfgrep='rg -z -F' 2>/dev/null
-
 # Zoxide is handling cd
 alias cd='cd' 2>/dev/null
-
-# Fd for find
-alias find='fd' 2>/dev/null
 
 # Bat for cat
 alias cat='bat -pp' 2>/dev/null
