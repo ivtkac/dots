260decf000edb35eb1472f5fabee126dc41e28a1
Ivan Tkachuk <ivtkac@gmail.com>
2024-06-21 17:01:45 +0300

    add alias `la` for ls -a

--- .config/bash/bashrc.d/bash_aliases.sh
+++ .config/bash/bashrc.d/bash_aliases.sh
@@ -6,6 +6,7 @@
 alias ls='eza' 2>/dev/null
 alias l.='eza -d .*' 2>/dev/null
 alias ll='eza -l --icons=auto --group-directories-first' 2>/dev/null
+alias la='ls -a'
 alias l1='eza -1'
 
 # Zoxide is handling cd
