bbe39a53f5d788bf103c4f1e3e733f69d095e09a
Ivan Tkachuk <ivtkac@gmail.com>
2024-06-23 05:14:58 +0300

    WIP: Add neovim config

---
+++ .config/nvim/lua/plugins/configs/mason.lua
@@ -0,0 +1,9 @@
+return {
+  ensure_installed = {
+    "pyright",
+    "ruff_lsp",
+    "ruff",
+    "lua-language-server",
+    "stylua",
+  },
+}
---
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    event = { "BufReadPost", "BufNewFile" },
+    cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
+    build = ":TSUpdate",
+    dependencies = {
+      "windwp/nvim-ts-autotag",
+    },
+    opts = function()
+      return require("plugins.configs.treesitter")
+    end,
+    config = function(_, opts)
+      require("nvim-treesitter.configs").setup(opts)
+    end,
+  },
+}
