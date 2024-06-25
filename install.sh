975de8d45e9c0cf50efb0f2342a124eaff933294
Ivan Tkachuk <ivtkac@gmail.com>
2024-06-25 05:55:00 +0300

    Update script to install flatpaks

--- install.sh
+++ install.sh
@@ -1,3 +1,22 @@
 #!/usr/bin/bash
 
 stow --target $HOME .
+
+flatpaks_to_install=$(cat installed_flatpaks.txt)
+
+read -p "Do you want to install flatpak applications? (y/n) " choice
+case "$choice" in
+y | Y)
+  echo "Installing flatpaks..."
+
+  for flatpak in $flatpaks_to_install; do
+    if ! flatpak list --app | grep -q "$flatpak"; then
+      echo "Installing $flatpak..."
+      flatpak install -y "$flatpak"
+    fi
+  done
+
+  echo "Flatpak installations complete!"
+  ;;
+*) ;;
+esac
