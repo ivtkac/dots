fc5c8e2de80234b637321a79b2874f1b9815e1fa
Ivan Tkachuk <ivtkac@gmail.com>
2024-07-19 14:57:58 +0300

    Update dots

--- .config/bash/bashrc.d/bash-aliases.sh
+++ .config/bash/bashrc.d/bash-aliases.sh
@@ -4,6 +4,7 @@
 
 # Eza for ls
 alias ls='eza' 2>/dev/null
+alias li='eza --icons'
 alias l.='eza -d .*' 2>/dev/null
 alias ll='eza -l --icons=auto --group-directories-first' 2>/dev/null
 alias la='ls -a'
