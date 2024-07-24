5fec6cb5c859c6b3e890d4068d9894f1418d1299
Ivan Tkachuk <ivtkac@gmail.com>
2024-07-24 09:34:59 +0300

    Update nvim config and fix install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/bash
 
-stow --target $HOME .
+stow --target "$HOME" .
 
 flatpaks_to_install=$(cat installed_flatpaks.txt)
 
-read -p "Do you want to install flatpak applications? (y/n) " choice
+read -r "Do you want to install flatpak applications? (y/n) " choice
 case "$choice" in
 y | Y)
   echo "Installing flatpaks..."
