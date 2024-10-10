06c7a12037db61a7000c41cc3f80c2384b7ee344
Ivan Tkachuk <ivtkac@gmail.com>
2024-10-10 17:06:47 +0300

    Update dots

--- .config/fish/conf.d/aliases.fish
+++ .config/fish/conf.d/aliases.fish
@@ -3,50 +3,48 @@ abbr -a cat "bat -pp"
 abbr -a b "bat -pp"
 
 # Ripgrep aliases
-abbr -a grep "rg"
-abbr -a egrep "rg"
+abbr -a grep rg
+abbr -a egrep rg
 abbr -a fgrep "rg -F"
 abbr -a xzgrep "rg -z"
 abbr -a xzegrep "rg -z"
 abbr -a xzfgrep "rg -z -F"
 
 # Good aliases
-abbr -a f "fzf"
+abbr -a f fzf
 
 abbr -a l "eza --sort=size --all --header --long --group-directories-first --icons always --git -- $argv"
 
-abbr -a zj "zellij"
+abbr -a zj zellij
 
-abbr -a v "nvim"
-abbr -a vi "nvim"
-abbr -a vim "nvim"
+abbr -a v nvim
+abbr -a vi nvim
+abbr -a vim nvim
 
 abbr -a md "python -m textual"
 
-abbr -a code "zeditor"
-abbr -a hx "helix"
+abbr -a code zeditor
+abbr -a hx helix
 
-abbr -a neo "fastfetch"
+abbr -a neo fastfetch
 
 abbr -a less "less -R"
 
-abbr -a c "wl-copy"
+abbr -a c wl-copy
 
 abbr -a upd "sudo pacman -Syuv"
 abbr -a cpwd "pwd | tr -d '\n' | wl-copy"
 
 # Help aliases
-abbr -a help "tldr"
+abbr -a help tldr
 
 # Tree aliases
 abbr -a tree "tree -C"
 
-# Tmux aliases
-abbr -a tmux "zellij"
 
 # Navigation aliases
-abbr -a cd "to"
-abbr -a "-" "cd -"
+abbr -a cd to
+abbr -a - "cd -"
 abbr -a ".." "cd .."
 abbr -a "..." "cd ../.."
 abbr -a "...." "cd ../../.."
