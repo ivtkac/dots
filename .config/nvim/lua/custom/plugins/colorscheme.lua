23d9c0b39f007c647154ce237602963bc8339f02
Ivan Tkachuk <ivtkac@gmail.com>
2025-03-13 08:45:04 +0200

    I like adwaita them!!! :love:

--- .config/nvim/lua/custom/plugins/colorscheme.lua
+++ .config/nvim/lua/custom/plugins/colorscheme.lua
@@ -4,8 +4,17 @@ return {
     priority = 1000,
     "catppuccin/nvim",
     name = "catppuccin",
+    disabled = true,
     config = function()
       vim.cmd.colorscheme "catppuccin"
+    end,
+  },
+  {
+    "Mofiqul/adwaita.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme "adwaita"
     end,
   },
 }
