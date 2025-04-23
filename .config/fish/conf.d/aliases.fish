ce797f8f62e9088728822ace10d2271b17f78614
Ivan Tkachuk <ivtkac@gmail.com>
2025-04-23 11:07:44 +0300

    config(fish): Update fish config

--- .config/fish/conf.d/aliases.fish
+++ .config/fish/conf.d/aliases.fish
@@ -1,40 +1,42 @@
-# Bat aliases
+# Onechar shortcuts
 abbr -a b "bat -pp"
-
-# Good aliases
+abbr -a v nvim
 abbr -a f fzf
-
+abbr -a t tmux
+abbr -a g git
 abbr -a l "eza --sort=size --all --header --long --group-directories-first --icons always --git -- $argv"
 
-abbr -a zj zellij
+abbr -a please sudo
 
-abbr -a v nvim
+# ls aliases
+abbr -a ls 'ls --color=auto'
+abbr -a la 'ls -a'
+abbr -a l1 'ls -1'
+abbr -a ll 'ls -lAh'
+
+# Editors
+abbr -a zj zellij
+abbr -a ze zellij
+
+abbr -a hx helix
+abbr -a md 'python -m textual'
+
 abbr -a vi nvim
 abbr -a vim nvim
-
-abbr -a md "python -m textual"
-
-abbr -a code zeditor
-abbr -a hx helix
+abbr -a code nvim
 
 abbr -a neofetch fastfetch
 
+# Colors
+abbr -a grep 'grep --color=auto'
 abbr -a less "less -R"
 
-abbr -a c wl-copy
-
+# Essentials
 abbr -a upd "sudo pacman -Syuv"
 abbr -a cpwd "pwd | tr -d '\n' | wl-copy"
 
-# Tree aliases
-abbr -a tree "tree -C"
-
-# Navigation aliases
+# Easy navigate
 abbr -a cd to
-abbr -a - "cd -"
-abbr -a ".." "cd .."
-abbr -a "..." "cd ../.."
-abbr -a "...." "cd ../../.."
 
 # Git aliases
 abbr -a ,a "git add"
@@ -61,12 +63,36 @@ abbr -a ,sc "git switch -c"
 abbr -a ,b "git branch"
 abbr -a ,l "git log"
 abbr -a ,st "git stash"
+abbr -a ,cp "git cherry-pick"
+abbr -a ,cpno "git cherry-pick --no-commit"
 
 abbr -a gi "gh issue"
 abbr -a gil "gh issue list"
 abbr -a giv "gh issue view"
-abbr -a gr "gh pr"
+abbr -a gpr "gh pr"
 abbr -a grl "gh pr list"
 abbr -a grv "gh pr view"
 abbr -a grc "gh pr checkout"
-abbr -a gb "gh browse"
+abbr -a gbr "gh browse"
+
+abbr -a vm "vboxmanage startvm OracleLinux 9.5 --type headless 2>/dev/null; ssh vm"
+
+abbr -a chownme "sudo chown -R $USER:$USER"
+
+abbr -a chx "chmod +x"
+
+abbr -a fstor "sudo find / -type f -exec du -h {} + | sort -hr | head"
+abbr -a ducwd "du -sh * .[!.]* | sort -h"
+
+abbr -a lsdof "sudo lsof +L1"
+
+abbr -a psa "ps aux"
+abbr -a psf "ps auxf"
+abbr -a psmem "ps -eo pid,ppid,cmd,rss,stat,%mem,%cpu --sort=-%mem | head"
+
+abbr -a ff "find . -type f -iname"
+abbr -a fdir "find . -type d -iname"
+
+abbr -a batcheck "upower -i $(upower -e | grep BAT)"
+
+abbr -a cln "sudo pacman -Rns $(pacman -Qtdq)"
--- .config/fish/conf.d/shell.fish
+++ .config/fish/conf.d/shell.fish
@@ -2,7 +2,7 @@ set fish_greeting
 
 set VIRTUAL_ENV_DISABLE PROMPT "1"
 
-fish_vi_key_bindings
+# fish_vi_key_bindings
 
 eval "$(zoxide init fish --cmd z)"
 eval "$(starship init fish)"
