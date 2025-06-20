33dfa8a07f40281f376d1c6272a8c101376215ea
Ivan Tkachuk <ivtkac@gmail.com>
2025-06-20 04:16:59 +0300

    update dots

--- .config/fish/conf.d/shell.fish
+++ .config/fish/conf.d/shell.fish
@@ -2,7 +2,6 @@ if status is-interactive
     set -gx STARSHIP_CONFIG /etc/starship.toml
     set -gx LANG en_US.utf8
     eval "$(starship init fish)"
-    eval "$(atuin init fish)"
     eval "$(zoxide init fish --cmd cd)"
     set -gx HOST (hostname -s)
 end
