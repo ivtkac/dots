2021dbde7d7604d0c6b45f2e4df7cc8039584230
Ivan Tkachuk <ivtkac@gmail.com>
2025-08-19 16:58:53 +0300

    Update sync notes

--- .local/bin/sync_notes.sh
+++ .local/bin/sync_notes.sh
@@ -8,3 +8,11 @@ VAULT_DIR="${VAULT_DIR:-$HOME/Documents/Vault}"
 rsync -av --include="*/" --include="*.md" --exclude="*" "$NOTES_DIR/notes/" "$VAULT_DIR/"
 
 rsync -av "$NOTES_DIR/_attachments/" "$VAULT_DIR/_attachments/"
+
+pushd "$VAULT_DIR" || exit 1
+
+git add .
+git commit -m "vault backup: $(date "+%Y-%m-%d %H:%M:%S")"
+git push
+
+popd || exit 1
