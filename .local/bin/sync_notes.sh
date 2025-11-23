a645f9e6141eff8f789c0fc8c0549da05deb5846
Ivan Tkachuk <ivtkac@gmail.com>
2025-11-23 18:45:26 +0200

    Update sync notes

--- .local/bin/sync_notes.sh
+++ .local/bin/sync_notes.sh
@@ -7,10 +7,26 @@ OUTPUT_DIR="$HOME/Projects/notes"
 
 pushd "$OUTPUT_DIR" || exit 1
 
-uv run transfer_notes.py --input-dir="$INPUT_DIR" --output-dir="$OUTPUT_DIR/notes" --skip-tags=sexuality
+echo "Transfer notes to notes"
+
+uv run transfer_notes.py --input-dir="$INPUT_DIR" --output-dir="$OUTPUT_DIR/notes" --skip-tags="job-hunt"
+
+echo "Commit and push"
 
 git add .
 git commit -m "vault backup: $(date "+%Y-%m-%d %H:%M:%S")"
 git push
+
+# quartz
+
+echo "Transfer notes to quartz"
+
+uv run transfer_notes.py --input-dir="$INPUT_DIR" --output-dir="$HOME/Projects/quartz/content" --skip-tags="job-hunt"
+
+pushd "$HOME/Projects/quartz" || exit 1
+
+echo "Sync quartz"
+
+npx quartz sync
 
 popd || exit 1
