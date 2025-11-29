9ec6b504d7568ba4496f8cd7d9512a7c3dffedee
KD-6190 <gearxxed@gmail.com>
2025-11-29 21:41:44 +0200

    Rsync do better job than my script, but still so:c

--- .local/bin/sync_notes.sh
+++ .local/bin/sync_notes.sh
@@ -3,27 +3,13 @@
 set -euo pipefail
 
 INPUT_DIR="$HOME/Documents/Notes"
-OUTPUT_DIR="$HOME/Projects/notes"
+OUTPUT_DIR="$HOME/Projects/quartz"
 
 pushd "$OUTPUT_DIR" || exit 1
 
-echo "Transfer notes to notes"
-
-uv run transfer_notes.py --input-dir="$INPUT_DIR" --output-dir="$OUTPUT_DIR/notes" --skip-tags="job-hunt"
-
-echo "Commit and push"
-
-git add .
-git commit -m "vault backup: $(date "+%Y-%m-%d %H:%M:%S")"
-git push
-
-# quartz
-
 echo "Transfer notes to quartz"
 
-uv run transfer_notes.py --input-dir="$INPUT_DIR" --output-dir="$HOME/Projects/quartz/content" --skip-tags="job-hunt"
-
-pushd "$HOME/Projects/quartz" || exit 1
+rsync -av --delete --exclude={'journal','private','templates','bases','.obsidian','.git'} "$INPUT_DIR"/* "$OUTPUT_DIR"/content/
 
 echo "Sync quartz"
 
